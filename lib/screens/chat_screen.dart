f9b869eef0996560a1311921ec3d68eda31e5935
Amit Patil <amitgpatil215@gmail.com>
2020-08-23 21:07:30 +0530

    fetching snapshot of data from firestore database

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -10,6 +11,23 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text("this works"),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          //snapshot will give us real time data changes
+          // so that we can re render our UI
+          FirebaseFirestore.instance
+              .collection('chats/m0Ol7QhBEHbcs0qWDtoY/messeges')
+              .snapshots()
+              .listen(
+            (event) {
+              event.docs.forEach((element) {
+                print(element.data()['text']);
+              });
+            },
+          );
+        },
       ),
     );
   }
